6f9e2ba6d521846637123cb1af85fdb6d1396dd6
unknown <rubina@otonomis.com>
2019-03-06 17:25:24 +0545

    second commit

--- app/src/main/java/com/example/myapplication/ui/home/DashboardMainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/home/DashboardMainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapplication.ui.home
 
 import android.os.Bundle
 import com.example.myapplication.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.android.support.DaggerAppCompatActivity
 import timber.log.Timber
 
 class DashboardMainActivity : DaggerAppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.songs -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.movies -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.books -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
